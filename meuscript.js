a47a5fd147bb7a3e6bdc87e81d6cc48174e6dc8e
ana_mantella <146769187+anaclaramantellaa@users.noreply.github.com>
2025-10-20 21:41:18 -0300

    Add files via upload

---
+++ meuscript.js
@@ -0,0 +1,9 @@
+function exibir(){
+    document.getElementById('texto').innerHTML = 'Seu mouse está sobre o texto';
+}
+function ocultar(){
+    document.getElementById('texto').innerHTML ='';
+}
+function mudar(){
+    document.getElementById('corpo').innerHTML = '' ;
+}
